396067d269c416f1d2ee98354d280c78806d3968
Ralph Waldenmaier <ralph.waldenmaier@gmail.com>
2022-03-24 15:31:10 +0100

    T3 - Implemented (#2)
    * Adding tests; T3 preparation
    
    * T3 implemented
    
    * Adding more tests
    
    Co-authored-by: Ralph Waldenmaier <ralwal@amazon.de>

--- dh-sorter/sorter.py
+++ dh-sorter/sorter.py
@@ -1,35 +1,49 @@
+import imp
+with open('.config/t3.py', 'rb') as fp:
+    t3 = imp.load_module('.config', fp, '.config/__init__.py', ('.py', 'rb', imp.PY_SOURCE))  
+
 class EC2Instance:
     def __init__(self, type, vCPU):
         self.type = type
         self.family = self.__family()
+        self.size = self.__size()
         self.vCPU = vCPU
+
+    def getBlockSize(self):
+        if self.family == "t3":
+            return 1
+        return self.vCPU
 
     def __family(self):
         return self.type.rsplit(".")[0]
+
+    def __size(self):
+        return self.type.rsplit(".")[1]
 
 class CapacityProvider:
     def getCapacity(self, type):
         if type == "r5" or type == "r5b":
            return 96
 
-
-class DedicatedHost:    
+class Block:
     def __init__(self, type):
         self.type = type
         self.usage = 0
-        self.vCPU = CapacityProvider().getCapacity(type)
+        self.capacity = 0 # to be initialized by inherited class
         self.instances = []
 
-    # adds an instance to a host. returns False if instance has no capacity
+    # adds an instance to a block. returns False if block has no capacity
     def addInstance(self, instance):
-        if instance.vCPU <= self.__balance():
-            self.usage = self.usage + instance.vCPU
+        instanceSize = instance.getBlockSize()
+        if instanceSize <= self.__balance():
+            self.usage = self.usage + instanceSize
             self.instances.append(instance)
             return True
         else:
             return False
 
     def printUsage(self):
+        print("Block --------------")
         print("type: " + self.type)
         print("usage: " + str(self.usage))
         print("--instances--")
@@ -38,7 +52,58 @@ class DedicatedHost:
         print("--------------")
 
     def __balance(self):
-        return self.vCPU - self.usage
+        return self.capacity - self.usage
+
+class DedicatedHost(Block):    
+    def __init__(self, type):
+        super().__init__(type)
+        self.capacity = CapacityProvider().getCapacity(type)
+
+class T3DedicatedHost(DedicatedHost):
+    def __init__(self, type):
+        super().__init__(type)
+        self.blocks = []
+        self.capacity = t3.maxmemory
+        self.hostUsage = 0
+    
+    # get values from config
+
+    # add an instance to a T3 host. Bucketing must be respected
+    def addInstance(self, instance):
+        # try to place instance on an existing block
+        for block in self.blocks:
+            if block.addInstance(instance) == True:
+                return True
+
+        # if no existing block, place instance on a new block if possible on existing DH
+        if self.__blockCapacityAvailable(instance.size) == True:
+            block = Block(instance.size)
+            block.capacity = self.__getBlockCapacity(instance.size)
+            self.blocks.append(block)
+            block.addInstance(instance)
+            self.hostUsage = self.hostUsage + self.__blockMemory(instance.size)
+            return True
+        
+        return False
+
+    def printUsage(self):
+        print("T3 Host --------------")
+        for block in self.blocks:
+            block.printUsage()
+        print("--------------")
+
+    def __blockCapacityAvailable(self, size):
+        # is there enough memory for a new block?
+        if self.capacity - self.hostUsage >= self.__blockMemory(size):
+            return True
+        return False
+
+    def __blockMemory(self, size):
+        mem = t3.blocksizes[size] * t3.memory[size]
+        return t3.blocksizes[size] * t3.memory[size]
+
+    def __getBlockCapacity(self, size):
+        return t3.blocksizes[size]
 
 class HostSorter:
     def __init__(self):
@@ -77,41 +142,21 @@ class HostSorter:
         self.__placeInstanceOnHost(instanceHosts, instance)
     
     def __placeInstanceOnHost(self, instanceHosts, instance):
-        placed = False
-
         # try to place instance on an existing host
         for host in instanceHosts:
             if host.addInstance(instance) == True:
-                placed = True
-                continue
+                return
 
         # if no existing host, place instance on a new host
-        if placed is False:
+        if instance.family == "t3":
+            newHost = T3DedicatedHost(instance.family)
+        else:
             newHost = DedicatedHost(instance.family)
-            instanceHosts.append(newHost)
-            newHost.addInstance(instance)
+        instanceHosts.append(newHost)
+        newHost.addInstance(instance)
+
 
     def printUsage(self):
         for hostFamily in self.hosts.values():
             for h in hostFamily:
                 h.printUsage()
-
-
-r5_2xl = EC2Instance("r5.2xlarge", 8)
-r5_12xl = EC2Instance("r5.12xlarge", 48)
-r5b_2xl = EC2Instance("r5b.8xlarge", 8)
-r5b_12xl = EC2Instance("r5b.12xlarge", 48)
-
-x = HostSorter()
-x.addInstance(r5_2xl)
-x.addInstance(r5_12xl)
-x.addInstance(r5_12xl)
-x.addInstance(r5_12xl)
-
-x.addInstance(r5b_2xl)
-x.addInstance(r5b_12xl)
-x.addInstance(r5b_12xl)
-x.addInstance(r5b_12xl)
-
-x.placeInstances()
-x.printUsage()
---
+++ dh-sorter/tests.py
@@ -0,0 +1,107 @@
+import unittest
+from sorter import EC2Instance 
+from sorter import HostSorter 
+
+class TestDedicatedHosts(unittest.TestCase):
+    def setUp(self):
+        self.r5_2xl = EC2Instance("r5.2xlarge", 8)
+        self.r5_12xl = EC2Instance("r5.12xlarge", 48)
+        self.r5b_2xl = EC2Instance("r5b.8xlarge", 8)
+        self.r5b_12xl = EC2Instance("r5b.12xlarge", 48)
+
+    def test_1r5_host(self):
+        x = HostSorter()
+        x.addInstance(self.r5_12xl)
+        x.addInstance(self.r5_12xl)
+        x.placeInstances()
+        self.assertEqual(len(x.hosts["r5"]), 1, "Should be 1 host")
+
+    def test_2r5_host(self):
+        x = HostSorter()
+        for i in range(3):
+            x.addInstance(self.r5_12xl)
+        x.placeInstances()
+        self.assertEqual(len(x.hosts["r5"]), 2, "Should be 1 host")
+
+    def test_6r5_host(self):
+        x = HostSorter()
+        for i in range(11):
+            x.addInstance(self.r5_12xl)
+        x.placeInstances()
+        self.assertEqual(len(x.hosts["r5"]), 6, "Should be 6 hosts")
+
+class TestT3DedicatedHosts(unittest.TestCase):
+    def setUp(self):
+        self.t3_xl = EC2Instance("t3.xlarge", 4)
+        self.t3_2xl = EC2Instance("t3.2xlarge", 8)
+        self.t3_medium = EC2Instance("t3.medium", 2)
+        self.t3_small = EC2Instance("t3.small", 2)
+        self.t3_micro = EC2Instance("t3.micro", 2)
+        self.t3_nano = EC2Instance("t3.nano", 2)
+
+    def test_1t3_host(self):
+        x = HostSorter()
+        x.addInstance(self.t3_xl)
+        x.placeInstances()
+        self.assertEqual(len(x.hosts["t3"]), 1, "Should be 1 hosts")
+
+    def test_1mixedt3_host(self):
+        x = HostSorter()
+        for i in range(16):
+            x.addInstance(self.t3_nano)
+        for i in range(40):
+            x.addInstance(self.t3_micro)
+        for i in range(40):
+            x.addInstance(self.t3_small)
+        for i in range(20):
+            x.addInstance(self.t3_2xl)
+        x.placeInstances()
+        self.assertEqual(len(x.hosts["t3"]), 1, "Should be 1 hosts")
+
+    def test_1mixedt3_host2(self):
+        x = HostSorter()
+        for i in range(104):
+            x.addInstance(self.t3_micro)
+        for i in range(40):
+            x.addInstance(self.t3_small)
+        for i in range(16):
+            x.addInstance(self.t3_medium)
+        for i in range(16):
+            x.addInstance(self.t3_2xl)
+        x.placeInstances()
+        self.assertEqual(len(x.hosts["t3"]), 1, "Should be 1 hosts")
+
+    def test_1mixedt3_host3(self):
+        x = HostSorter()
+        for i in range(64):
+            x.addInstance(self.t3_micro)
+        for i in range(12):
+            x.addInstance(self.t3_xl)
+        for i in range(16):
+            x.addInstance(self.t3_2xl)
+        x.placeInstances()
+        self.assertEqual(len(x.hosts["t3"]), 1, "Should be 1 hosts")
+
+    def test_2mixedt3_host(self):
+        x = HostSorter()
+        for i in range(16):
+            x.addInstance(self.t3_nano)
+        for i in range(40):
+            x.addInstance(self.t3_micro)
+        for i in range(40):
+            x.addInstance(self.t3_small)
+        for i in range(22):
+            x.addInstance(self.t3_2xl)
+        x.placeInstances()
+        self.assertEqual(len(x.hosts["t3"]), 2, "Should be 2 hosts")
+
+    def test_2mixedt3_host2(self):
+        x = HostSorter()
+        for i in range(48):
+            x.addInstance(self.t3_2xl)
+        x.placeInstances()
+        self.assertEqual(len(x.hosts["t3"]), 2, "Should be 2 hosts")
+
+
+if __name__ == '__main__':
+    unittest.main()
